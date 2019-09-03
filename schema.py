a846e1b55b6497e6397a99ccfeece40fb9a96546
Sam Dillard <sam@influxdata.com>
2019-09-03 13:24:29 -0700

    added create_user mutation and made uid=501(samdillard) gid=20(staff) groups=20(staff),12(everyone),61(localaccounts),79(_appserverusr),80(admin),81(_appserveradm),98(_lpadmin),33(_appstore),100(_lpoperator),204(_developer),250(_analyticsusers),395(com.apple.access_ftp),398(com.apple.access_screensharing),399(com.apple.access_ssh),701(com.apple.sharepoint.group.1) and  automatically assigned values

--- schema.py
+++ schema.py
@@ -3,11 +3,12 @@ import graphene
 import json
 from datetime import datetime
 import random
+import uuid
 
 class User(graphene.ObjectType):
-    id = graphene.ID()
+    id = graphene.ID(default_value=str(uuid.uuid4()))
     username = graphene.String()
-    created_at = graphene.DateTime()
+    created_at = graphene.DateTime(default_value=datetime.now())
 
 #create query type
 class Query(graphene.ObjectType):
@@ -29,20 +30,35 @@ class Query(graphene.ObjectType):
             User(id="1", username="Fred", created_at=datetime.now()),
             User(id="2", username="Bob", created_at=datetime.now())
         ],limit)
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(User)
+    #passing in arguments to Mutations requires an inner class of Arguments
+    class Arguments:
+        username = graphene.String()
 
-#set Query type to Schema and save to schema variable
-schema = graphene.Schema(query=Query)
+    def mutate(self, info, username):
+        user = User(username=username)
+        return CreateUser(user=user)
 
+class Mutation(graphene.ObjectType):
+    #CreateUser is a class we define that inherents from this class
+    create_user = CreateUser.Field()
 
+#set schema types to Schema and save to schema variable
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
+#graphene.Schema has a method `execute` that runs a query based on a query string you pass it
 result = schema.execute(
     #even though graphene requires snake case for the resolver functions, graphql requires camel case
     #note 'is_admin' is called with 'isAdmin'
     '''
-    {
-        users(limit: 1) {
-            id
-            username
-            createdAt
+    mutation {
+        createUser(username: "Jeff") {
+            user {
+                id
+                username
+                createdAt
+            }
         }
     }
     '''
@@ -51,7 +67,7 @@ result = schema.execute(
 #print odict of items in result
 print(result.data.items())
 #print result of using the 'hello' operation--the value of the field 'hello', in this case
-print(result.data['users'])
+#print(result.data['users'])
 #print the dict of items in json
 dictResult = dict(result.data.items())
 print(json.dumps(dictResult, indent=2)) #setting an indent makes json much more readable
